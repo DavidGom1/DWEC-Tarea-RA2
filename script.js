fa87131f5fc78ffc285e1c6110ee9dc19d5268b2
David Gómez Lozano <125882624+DavidGom1@users.noreply.github.com>
2025-10-08 16:28:15 +0200

    Add initial HTML and JavaScript files for DWEC - Tarea 2

---
+++ script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    let name = prompt("Hola, podrias decirnos tu nombre?");
+    console.log(name)
+    while(name === null || name.trim() === ""){
+        name = prompt("Tu nombre no puede estar vacío, tu nombre?");
+    }
+    let age = prompt("Y tu edad?");
+    while(age === null || age.trim() === ""){
+        age = prompt("Enserio, necesitamos tu edad...")
+    }
+
+    window.alert("Hola " + name + ", tienes " + age + " años, eres " + (age >= 18 ? "mayor" : "menor") + " de edad.");
+}
